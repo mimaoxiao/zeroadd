2cc7d7a374859a08cc2e3b190232893478f5320b
Kafuu Chino <masterspark@openingsource.org>
2019-09-17 16:43:42 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainControl from './component/MainControl';
+
+ReactDOM.render(<MainControl />, document.getElementById('root'));
